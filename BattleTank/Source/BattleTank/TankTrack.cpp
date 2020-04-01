804b5ffb09855756f7e932323867e0392a367ed9
Tabitha Farnsworth <tabitha.farnsworth@gmail.com>
2020-04-01 15:26:34 -0700

    Added quit button to main menu UI, added Tank Track custom component for controlling tracks, added basic force application for driving the tank

---
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+
+	// TODO clamp throttle value so player can't over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * MaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
+
